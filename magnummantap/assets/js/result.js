4157b8b6c1970ad06a836be1a1649594a98f8246
Asep Irman <asepirman@antigravity.id>
2021-01-15 16:57:00 +0700

    change plyr

--- magnummantap/assets/js/result.js
+++
@@ -1,5 +0,0 @@
-var statement = document.getElementById('statement');
-var result = localStorage.getItem("result");
-
-
-statement.innerHTML = '<img src="assets/images/Result/Result_'+result+'.jpg">';
